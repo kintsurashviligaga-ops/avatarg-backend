c452f4d5953ef207901a59bc5ec54722ccf52744
Giorgi kintsurashvili <kintsurashviligaga@gmail.com>
2026-01-05 22:04:22 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Avatar G Backend",
+  description: "Backend is running"
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
