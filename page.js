f076edad2417770857b2a00f9632a14643445fb6
Giorgi kintsurashvili <kintsurashviligaga@gmail.com>
2026-01-06 09:42:33 +0400

    Delete page.js

--- page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>Avatar G Backend ✅</h1>
-      <p>Backend is running</p>
-      <a href="/api/health">/api/health</a>
-    </main>
-  );
-}
