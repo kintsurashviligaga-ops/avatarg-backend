c9352a1a0a11e108b8a5c2d38ac9298d656a6270
Giorgi kintsurashvili <kintsurashviligaga@gmail.com>
2026-01-08 15:00:08 +0400

    Update route.js

--- app/api/health/route.js
+++ app/api/health/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { withCORS, corsOPTIONS } from "../_utils/cors";
+import { withCORS, corsOPTIONS } from "../utils/cors";
 
 export async function GET(req) {
   return withCORS(
