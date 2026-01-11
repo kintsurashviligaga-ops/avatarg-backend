0a2d931ac5f201938387eb62bef54a04b60566f5
Giorgi kintsurashvili <kintsurashviligaga@gmail.com>
2026-01-11 19:16:31 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // ყველა API route
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // თუ გინდა, მერე შეცვლი კონკრეტულ domain-ზე
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
