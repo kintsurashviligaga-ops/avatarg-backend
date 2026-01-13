4463770ec2a731707e30e06c887b457a46dc5932
Giorgi kintsurashvili <kintsurashviligaga@gmail.com>
2026-01-13 15:54:13 +0400

    Update route.ts

--- app/api/music/status/route.ts
+++ app/api/music/status/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { supabaseAdmin } from "../../../../lib/supabaseAdmin";
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 /**
  * CORS:
  * - credentials-ით "*" არ შეიძლება
